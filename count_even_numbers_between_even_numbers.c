2b5cdab8380b87d9c914ad9e52afaaeb4b61e780
BALAJI REDDY <84330632+Balajireddy2002@users.noreply.github.com>
2022-05-05 16:03:27 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     cin>>n;     int arr[n];     int count=0;     for(int i=0;i<n;i++){         cin>>arr[i];     }     for(int i=0;i<n-2;i++){         if((arr[i]%2==0 and arr[i+2]%2==0)){             if(arr[i+1]%2==0)count++;         }     }     cout<<count; }

---
+++ count_even_numbers_between_even_numbers.c
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int count=0;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-2;i++){
+        if((arr[i]%2==0 and arr[i+2]%2==0)){
+            if(arr[i+1]%2==0)count++;
+        }
+    }
+    cout<<count;
+}
